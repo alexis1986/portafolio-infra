bda5ff86bf49a99e569973fc9c4fea4dca47b820
alexis1986 <alexis.castellano@gmail.com>
2025-10-01 12:03:21 -0300

    fix in endpoint field

--- provider.tf
+++ provider.tf
@@ -9,18 +9,13 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "alexdevvv-portafolio-terraform-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1" # DigitalOcean Spaces usa este valor por convención
-
-    endpoints = {
-      s3 = "https://nyc3.digitaloceanspaces.com"
-    }
-
+    bucket                      = "alexdevvv-portafolio-terraform-state"
+    key                         = "terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "https://nyc3.digitaloceanspaces.com"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
   }
 }
 
